4f926677b778d82c659a7853f2e0f82a36b0e0fa
Dan Rees-Jones <revdan@dan-rees-joness-imac-2.local>
2013-06-29 10:48:40 +0100

    turbolinks fades

--- config.ru
+++ config.ru
@@ -1,4 +1,4 @@
 require "rack/jekyll"
 require 'newrelic_rpm'
-
+use Rack::Deflater
 run Rack::Jekyll.new(:destination => 'public')
--- js/js.js
+++ js/js.js
@@ -1,7 +1,1 @@
-$(document).on('page:fetch', function() {
-  return $('#content').fadeOut('fast');
-});
 
-$(document).on('page:restore', function() {
-  return $('#content').fadeIn('fast');
-});
